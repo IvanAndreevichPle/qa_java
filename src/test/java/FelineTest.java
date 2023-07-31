65e3b362de60349506ae5a922ff3c83826ac35c0
Ivan Plekhanov <dr.plekhanov@gmail.com>
2023-07-31 15:03:19 +0300

    feat new tests added

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class FelineTest {
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Test
+    public void testGetFamilyReturnsCats() {
+        Feline feline = new Feline();
+        String result = feline.getFamily();
+        assertEquals("Кошачьи", result);
+    }
+
+    @Test
+    public void testGetKittens() {
+        Feline feline = new Feline();
+        int kittens = feline.getKittens();
+        assertEquals(1, kittens);
+    }
+
+    @Test
+    public void testGetKittensWithArgument() {
+        Feline feline = new Feline();
+        int kittens = feline.getKittens(3);
+        assertEquals(3, kittens);
+    }
+
+    @Test
+    public void testEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> mealList = List.of("Животные", "Птицы", "Рыба");
+        List<String> food = feline.eatMeat();
+        assertEquals(food, mealList);
+    }
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class LionTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testGetKittens() throws Exception {
+        Lion lion = new Lion("Самец", new Feline());
+        int kittens = lion.getKittens();
+        assertEquals(1, kittens);
+    }
+
+    @Test
+    public void testEatMeat() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+
+        List<String> mealList = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(mealList);
+
+        List<String> food = lion.getFood();
+        assertEquals(food, mealList);
+    }
 }
